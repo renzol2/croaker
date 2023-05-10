61a4cc184eb2b0d5813da674aee3ade3f2799e99
Renzo <renzol2@illinois.edu>
2023-05-10 10:22:20 -0500

    Revert name/VST3 ID and update package version

--- src/lib.rs
+++ src/lib.rs
@@ -462,7 +462,7 @@ impl Croaker {
 }
 
 impl Plugin for Croaker {
-    const NAME: &'static str = "croaker reverb test";
+    const NAME: &'static str = "croaker";
     const VENDOR: &'static str = "renzofrog";
     const URL: &'static str = "https://www.renzofrog.com";
     const EMAIL: &'static str = "renzomledesma@gmail.com";
@@ -676,7 +676,7 @@ impl ClapPlugin for Croaker {
 }
 
 impl Vst3Plugin for Croaker {
-    const VST3_CLASS_ID: [u8; 16] = *b"renzofrogcroake3";
+    const VST3_CLASS_ID: [u8; 16] = *b"renzofrogcroaker";
 
     const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
         &[Vst3SubCategory::Fx, Vst3SubCategory::Distortion];
